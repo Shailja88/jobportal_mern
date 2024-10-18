116ec381ab19b0cb998caea9ed572e7187f1cda5
Shailja <genieshailja@gmail.com>
2024-10-18 21:28:33 +0530

    production ready webapp2

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportal-mern-ghz6.onrender.com/',
     credentials:true
 }
 
